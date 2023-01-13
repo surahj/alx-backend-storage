ab3594f3dcf9341f944bd1bda570b0d4b1a02aad
surahj <odebodeopeyemi@gmail.com>
2023-01-13 02:45:55 +0000

    Fix error

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,3 @@
--- script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name,
-          IFNULL(split,2020) - IFNULL(formed,0) AS lifespan
-FROM metal_bands
-WHERE style like '%Glam rock%'
-ORDER BY 2 DESC;
+-- SQL script that lists all bands with
+--   Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
