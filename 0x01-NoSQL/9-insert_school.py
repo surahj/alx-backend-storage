4832662387c311266db0f6562382315f0fe349bc
surahj <odebodeopeyemi@gmail.com>
2023-01-16 09:58:37 -0800

    Create 9-insert

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Insert a document in Python"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    function that inserts a new document
+    in a collection based on kwargs
+    """
+    doc = mongo_collection.insert_one(kwargs)
+    return doc.inserted_id
