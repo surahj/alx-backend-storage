e69396b112ccc74ba2f515042ce1be3792988676
surahj <odebodeopeyemi@gmail.com>
2023-01-19 10:26:40 +0000

    Add count decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,20 @@ Redis basic.
 from typing import Union, Callable, Optional
 import redis
 import uuid
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """Creates and returns function that increments the count \
+        for that key every time the method is called and returns \
+        the value returned by the original method"""
+    method_key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(method_key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     """Cache class to handle redis operations."""
@@ -14,6 +27,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Takes and stores a data argument and returns a string."""
         key = str(uuid.uuid4())
